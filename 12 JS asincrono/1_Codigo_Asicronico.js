01a7f7b7034e0dbc3c6ec6ba2305ee33c3db4c00
Daniel <studiosdopplermusic@gmail.com>
2022-04-08 21:01:00 -0500

    Promesas

---
+++ 12 JS asincrono/1_Codigo_Asicronico.js
@@ -0,0 +1,13 @@
+// ASYNC
+
+    // 1. RECURSOS..............................
+        
+        // Sincronico, es cuando la ejecución del codigo se da linea por linea en el orden que está escrita nuestra aplicación, esperando que la linea anterior termine de ejecutarse
+            
+            // - Se pueden tener problemas cuando una linea tarda demasiado en ejecutarse. 
+
+        // Asincronico (No bloqueante) , es cuando la ejecución de una parte del codigo se ejecuta en segundo plano sin parar la ejecución del programa.
+
+            // - Se pospone una acción en el futuro para que el codigo no se bloquee
+            // - Los artibutos de las etiquetas se ejecutan de manera asincronica, por ejemplo al cargar una imagen, para que no se deba de esperar todo el tiempo de carga (en este caso cuando un elemento termina de cargarse ejecuta el evento load)
+
---
+++ 12 JS asincrono/2_Ajax_Y_API.js
@@ -0,0 +1,16 @@
+// AJAX JavaScript y XML asíncronos 
+    // XML era un formato de datos que solia ser usado para transmitir datos en la web, a pesar de que hoy en dia ya no es tan usado
+    // Hoy en dia es mas usado el formato JSON, y es un objeto JS convertido en una cadena
+    // Nos permite comunicarnos con servidores web remotos de forma asincróna 
+    // En la práctica hacemos llamadas Ajax en nuestro código para solicitar algunos datos de un servidor web de forma dinámica, pudiendo acceder a datos sin recargar la página 
+
+    // 1. Con AJAX podemos hacer una petición HTTP a servidores (de manera asincrona)
+    // 2. El servidor web devolverá los datos solicitados (de manera asincrona)
+    // 3. El Servidor contiene una API que contiene los datos que estamos solicitando 
+
+
+// API Interface de Programación De aplicaciones (Application Programming Interface)
+    // Permite comunicación entre aplicaciones 
+    // Son piezas de software autonomas 
+    // Las API usadas con AJAX suelen ser las API ONLINE 
+        // Una API Online es una app que se ejecuta en un servidor, recibe solicitudes de datos, recupera esos datos y los envia al cliente 
