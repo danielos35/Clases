00c4bf84a91454050bf78b2d512ffc8cb734d087
Daniel <studiosdopplermusic@gmail.com>
2022-04-26 21:40:48 -0500

    Programación funcional

---
+++ 13 JS moderno/6_codigoModerno.js
@@ -0,0 +1,26 @@
+// Codigo moderno
+
+    // Nunca usar Var.
+    // Refactorizar siempre que sea posible.
+    
+// Funciones
+
+    // Cada función debe de hacer una sola cosa
+    // NO utilizar mas de tres parametros en una función
+    // Devolver el mismo tipo de dato ingresado
+    // Usar funciones flecha siempre y cuando hagan el codigo mas legible 
+
+// OOP 
+    // Usar ES6 Clases
+    // Encapsular
+
+// Intentar NO ANIDAR CODIGO
+// USAR IF, en lugar de if else 
+// Evitar el uso de ciclos for 
+
+
+
+// Codigo Asincrono
+    // Consumir promesas usando siempre Async y await   
+    // Realizar preferiblemente las consultas en paralelo
+    // Manejar siempre los errores
---
+++ 13 JS moderno/7_codigoDeclarativoYFuncional.js
@@ -0,0 +1,19 @@
+// Paradigmas 
+
+    // 1. Imperativo
+        // Declarar cada uno de los pasos que deberá realizar el programa.
+
+
+    // 2. Declarativo
+        // Se indica que debe a hacer mas no el como
+
+// Pradigma Funcional
+    // Es un subparadigma de la programación declarativa 
+    // Escribir programas mediante el uso de multiples funciones puras
+
+    // CARACTERISTICAS
+        // 1. NO SIDE EFECT, No se debe de modificar ninguno de los datos fuera de una función, No mostrar nada en la consola, o cambios en el DOM 
+
+        // 2. FUNCIÓN PURA, es una función sin efectos secundarios, que no muta ninguna variable externa y que tampoco depende de ninguna variable externa
+
+        // 3. INMUTABILIDAD, los datos nunca serán modificados 
