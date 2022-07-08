7b7fba00b704ebdcac71f63a1f742cb171c305f9
Daniel <studiosdopplermusic@gmail.com>
2022-07-07 20:38:43 -0500

    corto circuito repaso

---
+++ 99_Repaso/01_dataStructure/05_corto_circuito.ts
@@ -0,0 +1,21 @@
+/*
+
+Operadores logicos 
+- Compuertas logicas
+
+*/ 
+
+// El primer elemento que exista; 
+let numero =  null || 12 || null; 
+console.log(numero);
+
+// Retorna primer valor que sea falso (0, undefind, '', null) o ultimo valor en existir; 
+console.log(0 && '' && null);
+console.log(12 && 'HolaMundo' && null);
+
+
+// Si un valor existel, asigna el siguiente valor;
+let esFalso = true && 'HolaMundo'; 
+console.log(esFalso);
+
+
