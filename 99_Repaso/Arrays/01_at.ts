16b6d1897cbde8f328bd48fbaf9675c33ba7b73d
Daniel <studiosdopplermusic@gmail.com>
2022-09-29 18:45:42 -0500

    ForEach

---
+++ 99_Repaso/Arrays/01_at.ts
@@ -0,0 +1,2 @@
+let arregloNumeros: Array<number> = [12, 3, 4, 5];
+// console.log(arregloNumeros.at(1));
---
+++ 99_Repaso/Arrays/02_forEach.ts
@@ -0,0 +1,3 @@
+let arrayNumber: Array<number> = [1, 2, 3, 4, 5, 6];
+
+arrayNumber.forEach((res, index, array) => console.log(res, index, array));
