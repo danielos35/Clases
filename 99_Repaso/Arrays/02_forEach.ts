0dda37a6cf97e01459b821ef0c552f59d614fa3b
Daniel <studiosdopplermusic@gmail.com>
2022-09-29 19:32:10 -0500

    mapas

--- 99_Repaso/Arrays/02_forEach.ts
+++ 99_Repaso/Arrays/02_forEach.ts
@@ -1,3 +1,10 @@
 let arrayNumber: Array<number> = [1, 2, 3, 4, 5, 6];
-
 arrayNumber.forEach((res, index, array) => console.log(res, index, array));
+
+// Iterar Maps
+let mapa: any = new Map<boolean, string>();
+mapa.set(true, "Hola Mundo");
+mapa.set(false, "Hola Mundo");
+
+let isReal = true;
+console.log(mapa.get(isReal));
