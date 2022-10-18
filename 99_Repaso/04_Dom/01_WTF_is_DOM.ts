b2da10976549a4926f263983a5aabe835d48f84a
Daniel <studiosdopplermusic@gmail.com>
2022-10-17 20:29:52 -0500

    WTF is DOM

---
+++ 99_Repaso/04_Dom/01_WTF_is_DOM.ts
@@ -0,0 +1,22 @@
+/*
+
+DOM
+
+-- Document Object Model: 
+
+- Permite manipular el el HTML y sus estilos por medio de JS 
+- Es un punto de conexión entre HTML y JS
+- Todos los elementos del DOM son un Objeto en especifico
+
+ESTRUCTURA DEL DOM: 
+
+-- DOCUMENT (por eso se utilza el selector como document.querySelector("..."))
+-- HTML 
+-- Head
+-- body
+-- title
+-- section 
+ETC ...
+
+DOM hace parte de algo llamado web API, que son bibliotecas a las que podemos acceder por medio de JS 
+*/ 
