3b2dc9a95b1c256669cd2dae5e48d62a972c49e8
Daniel <studiosdopplermusic@gmail.com>
2022-10-19 22:01:59 -0500

    what is JS

---
+++ 99_Repaso/05_JS/01_over_view.ts
@@ -0,0 +1,28 @@
+/*
+
+JAVASCRIPT
+
+- Lenguage High level: 
+-- No necesita administrar directamente los recursos del equipo donde se ejecuta
+
+- Garbage-collected
+-- Elimina automaticamente los objetos que son utilizados en ese momento de la memoria
+
+- lenguage interpretado o compilado en tiempo real
+-- JavaScript es una abstracción sobre el lenguage maquina (binario)
+
+- Lenguage multiparadigma 
+-- Procedural 
+-- OOP 
+-- FP
+
+- Prototype
+-- Todo el JS es un objeto excepto los valores primitivos como numeros, strings
+
+- Es un lenguaje multiparadigma basado en prototipos, interpretado o just-time-compiled
+- Es un lenguage de un solo proceso
+
+
+
+
+*/ 
