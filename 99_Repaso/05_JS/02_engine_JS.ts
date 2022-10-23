282c047841588594e4ec8c9412dc88a5091eff66
Daniel <studiosdopplermusic@gmail.com>
2022-10-22 21:51:58 -0500

    Motor JS

---
+++ 99_Repaso/05_JS/02_engine_JS.ts
@@ -0,0 +1,27 @@
+/*
+
+JavaScript Engine
+
+- Es un programa de computadora que ejecuta JS
+- Cada navegador tiene su motor JS, el mas conocido es el de Chrome, llamado V8 
+
+PARTES DE UN MOTOR JS
+
+- CALL STACK 
+-- Es donde nuestro codigo se ejecuta usando 'execution context'
+
+- HEAP
+-- Espacio de memoria donde se almacenan los objetos necesarios para la ejecución de JS de manera no estructurada
+
+
+
+NOTAS:
+
+Interpretación: El codigo se lee y se ejecuta al mismo tiempo 
+Compilación: Todo el codigo se convierte en codigo maquina
+
+
+El motor de JavaScript es un Mix entre un lenguaje compilado e interpretado 
+
+
+*/ 
