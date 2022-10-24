bf9382241a9b1602179e65d2c250d2de23d8d8d8
Daniel <studiosdopplermusic@gmail.com>
2022-10-24 15:22:06 -0500

    execution js

---
+++ 99_Repaso/05_JS/03_execution.ts
@@ -0,0 +1,34 @@
+/*
+
+PASOS DE LA EJECUCIÓN DE JAVASCRIPT 
+
+1 - Creación de contexto global de ejecución 
+-- Se ejecutará todo el codigo que esté fuera de las funciones
+-- En cualquier aplicativo JS solo existe un contexto de ejecución 
+(ver contexto de ejecucion mas abajo)
+
+2 - La computadora ejecuta el codigo 
+
+3 - Ejecución de las funciones 
+-- También se incluyen los metodos, ya que estos son simplemente funciones enlazadas a un objeto
+
+4 - El motor JS espera de las callback functions
+
+
+CONTEXTO DE EJECUCIÓN
+
+- Contexto de ejecución: Lugar donde se ejecuta cierto codigo
+- Contexto de ejecución objeto: un objeto crea su propio contexto de ejecución
+- this: hacer referencia al contexto de ejecución  
+
+CALL STACK 
+
+- 
+
+NOTAS
+
+-
+
+
+
+*/ 
