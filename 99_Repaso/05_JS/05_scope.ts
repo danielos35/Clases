2612531e9d33be316f7b9f5592683e67a628378b
Daniel <studiosdopplermusic@gmail.com>
2022-10-25 21:13:25 -0500

    scope

---
+++ 99_Repaso/05_JS/05_scope.ts
@@ -0,0 +1,22 @@
+/*
+
+SCOPING 
+- Lugar doonde pueden ser llamadas las variables ('Donde estas viven')
+
+LEXICAN SCOPING 
+- El scope depende de la ubicación de las variable
+
+SCOPE
+- Entorno donde la variable es declarada
+
+SCOPES
+-- Global scope
+-- function scope
+-- block scope
+
+SCOPE OF A VARIABLEs
+-- Entorno donde la variable va poder ser usada
+
+
+
+*/ 
