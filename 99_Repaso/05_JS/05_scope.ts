00c301f8c08fee33d6b7131f4a3f68b5e06c65ed
Daniel <studiosdopplermusic@gmail.com>
2022-10-26 19:49:10 -0500

    context execution

--- 99_Repaso/05_JS/05_scope.ts
+++ 99_Repaso/05_JS/05_scope.ts
@@ -1,5 +1,11 @@
 /*
 
+
+EXECUTION CONTEXT
+- varaible enviroment
+- scope chain
+- this keyword
+
 SCOPING 
 - Lugar doonde pueden ser llamadas las variables ('Donde estas viven')
 
@@ -16,6 +22,20 @@ SCOPES
 
 SCOPE OF A VARIABLEs
 -- Entorno donde la variable va poder ser usada
+-- Las funciones son valores que se almacenan en variables
+
+
+BLOCK SCOPE 
+-- Son todas las variables que están dentro de un bloque de codigo (entre llaves)
+
+
+NOTES: 
+
+- Las variables globales estarán disponibles en todos los scopes inferiores.
+- la busquedad de variables se hace scope por scope hasta encontrar la coincidencia.
+- Todas las funciones crean un scope 
+- las variables declaradas con var, siempre estarán dentro del contexto global
+
 
 
 
