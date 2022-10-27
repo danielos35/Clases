fcf4db0da838cc0d41c5c2a34bc2b02ccfd9887c
Daniel <studiosdopplermusic@gmail.com>
2022-10-26 19:58:41 -0500

    hoisting review

---
+++ 99_Repaso/05_JS/06_hoisting.ts
@@ -0,0 +1,25 @@
+/*
+HOISTING 
+
+- Hace que algunos tipos de variables sean accesibles antes de estar realmente declaradas
+- El motor de JS busca las variables que estan declaradas
+
+TIPOS DE OBJETOS 
+
+- Funciones
+-- significa que podemos usar la funciones antes de ser declaradas
+
+- Variables declaradas con var
+-- También puden ser llamadas antes de ser declaradas
+
+let and const variables 
+-- Si sufren de hoisting, pero su valor se estable en <unintialialized>
+-- Estas variables se almacenan antes de su inicialización en la Temporal dead zone TDZ
+
+Arrow functions
+- se comportan segun con el prefijo que hayan sido inicializadas (var, let, const)
+
+
+
+
+*/ 
