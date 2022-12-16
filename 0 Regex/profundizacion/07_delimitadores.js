6a339a6cad72be016f69f3bd689ab50ac8ac526b
Daniel <studiosdopplermusic@gmail.com>
2022-12-15 23:36:29 -0500

    expresiones regulares #2

---
+++ 0 Regex/profundizacion/07_delimitadores.js
@@ -0,0 +1,17 @@
+/*
+
+*******
+.* todos los caracteres
+\d* todo lo que sea numeros (selecciona todos)
+\w* todo lo que sea caracteres (seleccionan todos)
+*******
+- Todos los caracteres 
+
+
+*/
+
+/**
+ *  * cero o muchos
+ *  + uno o muchos
+ *  ? cero o uno
+ * **/
